5063b108581739717799f56e896cc2a728bf1b5e
Listiger Lurch <listiger_lurch@icloud.com>
2024-04-17 01:21:41 +0200

    Bug fixes and basic alerts

--- logic.js
+++ logic.js
@@ -1,13 +1,23 @@
+// logic for the modal for the starting player
+
+let currentPlayer;
+const yellowButton = document.querySelector("#yellowStart");
+const blueButton = document.querySelector("#blueStart");
+yellowButton.addEventListener("click", () => {
+    currentPlayer = "playerYellow";
+});
+blueButton.addEventListener("click", () => {
+    currentPlayer = "playerBlue";
+});
+
+
 const cells = document.querySelectorAll(".cell");
-
-let currentPlayer = "playerYellow";
-
 
 // function for checking winning combinations
 const checkCombinations = (stepperIndex) => {
     let startRowIndex;       // starting cell iterating 
     let endRowIndex;         // ending cell iterating rows
-    let startColIndex;      // starting cell iterating columnns
+    let startColIndex;       // starting cell iterating columnns
     let endColIndex;         // ending cell iterating columnns
 
     switch (stepperIndex) {
@@ -46,13 +56,13 @@ const checkCombinations = (stepperIndex) => {
                 cells[i * 10 + j + stepperIndex].classList.contains("playerYellow") &&
                 cells[i * 10 + j + 2 * stepperIndex].classList.contains("playerYellow") &&
                 cells[i * 10 + j + 3 * stepperIndex].classList.contains("playerYellow")) {
-                document.querySelector("h1").innerHTML = "Yellow won";
+                alert("Yellow won!");
 
             } else if (cells[i * 10 + j].classList.contains("playerBlue") &&
                 cells[i * 10 + j + stepperIndex].classList.contains("playerBlue") &&
                 cells[i * 10 + j + 2 * stepperIndex].classList.contains("playerBlue") &&
                 cells[i * 10 + j + 3 * stepperIndex].classList.contains("playerBlue")) {
-                document.querySelector("h1").innerHTML = "Blue won";
+                alert("Blue won!");
             }
         }
     }
@@ -64,45 +74,71 @@ const removerBlue = document.querySelector("#removerBlue");
 let counterYellow = 3;
 let counterBlue = 3;
 removerYellow.addEventListener("click", () => {
-    removerYellow.classList.add("removerYellowOn");
-    counterYellow--;
-    if (counterYellow === 0) {
-        removerYellow.textContent = "No kills left!";
-        removerYellow.disabled = true;
+    if (currentPlayer === "playerYellow") {
+        removerYellow.classList.add("removerYellowOn");
+        counterYellow--;
+        if (counterYellow === 0) {
+            removerYellow.textContent = "No kills left!";
+            removerYellow.disabled = true;
+        } else {
+            removerYellow.textContent = `Kill a blue Square! (${counterYellow.toString()})`;
+        }
     } else {
-        removerYellow.textContent = `Kill a blue Square! (${counterYellow.toString()})`;
+        alert("Can't choose this option!");
     }
+
 });
 removerBlue.addEventListener("click", () => {
-    removerBlue.classList.add("removerBlueOn");
-    counterBlue--;
-    if (counterBlue === 0) {
-        removerBlue.textContent = "No kills left!";
-        removerBlue.disabled = true;
+    if (currentPlayer === "playerBlue") {
+        removerBlue.classList.add("removerBlueOn");
+        counterBlue--;
+        if (counterBlue === 0) {
+            removerBlue.textContent = "No kills left!";
+            removerBlue.disabled = true;
+        } else {
+            removerBlue.textContent = `Kill a yellow Square! (${counterBlue.toString()})`;
+        }
     } else {
-        removerBlue.textContent = `Kill a yellow Square! (${counterBlue.toString()})`;
+        alert("Can't choose this option!");
     }
+
 });
 
 // logic for clicking on cells
 cells.forEach((cell) => {
     cell.addEventListener("click", () => {
-        if (removerYellow.classList.contains("removerYellowOn") && cell.classList.contains("playerBlue")) {
-            cell.classList.add("unusable");
-            removerYellow.classList.remove("removerYellowOn");
-
-        } else if (removerBlue.classList.contains("removerBlueOn") && cell.classList.contains("playerYellow")) {
-            cell.classList.add("unusable");
-            removerBlue.classList.remove("removerBlueOn");
-
-        } else if (!(cell.classList.contains("playerYellow") || cell.classList.contains("playerBlue"))) {
-            cell.classList.add(currentPlayer);
-        }
-
-        if (currentPlayer === "playerYellow") {
-            currentPlayer = "playerBlue";
+        if (cell.classList.contains("unusable")) {
+            alert("Can't use killed squares! Please select other!");
         } else {
-            currentPlayer = "playerYellow";
+
+            if (currentPlayer === "playerYellow") {
+
+                if (removerYellow.classList.contains("removerYellowOn") && cell.classList.contains("playerBlue")) {
+                    cell.classList.add("unusable");
+                    cell.classList.remove("playerBlue");
+                    removerYellow.classList.remove("removerYellowOn");
+                    currentPlayer = "playerBlue";
+                } else if (!(cell.classList.contains("playerYellow") || cell.classList.contains("playerBlue"))) {
+                    cell.classList.add(currentPlayer);
+                    currentPlayer = "playerBlue";
+                } else if (cell.classList.contains("playerYellow") || cell.classList.contains("playerBlue")) {
+                    alert("Cant't play here!");
+                }
+
+            } else if (currentPlayer === "playerBlue") {
+
+                if (removerBlue.classList.contains("removerBlueOn") && cell.classList.contains("playerYellow")) {
+                    cell.classList.add("unusable");
+                    cell.classList.remove("playerYellow");
+                    removerBlue.classList.remove("removerBlueOn");
+                    currentPlayer = "playerYellow";
+                } else if (!(cell.classList.contains("playerYellow") || cell.classList.contains("playerBlue"))) {
+                    cell.classList.add(currentPlayer);
+                    currentPlayer = "playerYellow";
+                } else if (cell.classList.contains("playerYellow") || cell.classList.contains("playerBlue")) {
+                    alert("Cant't play here!");
+                }
+            }
         }
         checkCombinations(1);
         checkCombinations(10);
